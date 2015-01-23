46fd243b9c0d507adc585a36e9be4988af9c4985
George Starcher <george@georgestarcher.com>
2015-01-22 19:28:26 -0600

    updates indentions
    Updates indentions to four space standard

--- GoogleSpreadsheet/alert_to_gsheet.py
+++ GoogleSpreadsheet/alert_to_gsheet.py
@@ -28,107 +28,107 @@ logger.Formatter.converter = time.gmtime
 # Functions
 
 def versiontuple(v):
-        return tuple(map(int, (v.split("."))))
+    return tuple(map(int, (v.split("."))))
 
 def logDebug(s):
-        """ print any extra debug info """
-        if _DEBUG:
-                logger.info("script="+_MI_APP_NAME+" %s" % str(s))
+    """ print any extra debug info """
+    if _DEBUG:
+        logger.info("script="+_MI_APP_NAME+" %s" % str(s))
 
 def logError(s):
-        """ print any errors that occur """
-        logger.error("script="+_MI_APP_NAME+" %s" % str(s))
+    """ print any errors that occur """
+    logger.error("script="+_MI_APP_NAME+" %s" % str(s))
 
 def logAction(s):
-        """ log events to show normal activity of the alert script """
-        if _LOG_ACTION:
-                logger.info("script="+_MI_APP_NAME+" %s" % str(s))
+    """ log events to show normal activity of the alert script """
+    if _LOG_ACTION:
+        logger.info("script="+_MI_APP_NAME+" %s" % str(s))
 
 def getSplunkVersion(sessionKey):
-        """ function to obtain the Splunk software version. This is used to determine parsing of the sessionKey """
+    """ function to obtain the Splunk software version. This is used to determine parsing of the sessionKey """
 
-        from xml.dom import minidom
+    from xml.dom import minidom
 
-        base_url = 'https://localhost:8089'
+    base_url = 'https://localhost:8089'
 
-        request = urllib2.Request(base_url + '/services/server/info',None,headers = { 'Authorization': ('Splunk %s' %sessionKey)})
-        server_content = urllib2.urlopen(request)
-        serverDoc = minidom.parseString(server_content.read())
-        entryInfo = serverDoc.getElementsByTagName('entry')
-        key_nodes = entryInfo[0].getElementsByTagName('content')[0].getElementsByTagName('s:key')
-        nodes = filter(lambda node: node.attributes['name'].value == 'version', key_nodes)
-        version = nodes[0].firstChild.nodeValue
+    request = urllib2.Request(base_url + '/services/server/info',None,headers = { 'Authorization': ('Splunk %s' %sessionKey)})
+    server_content = urllib2.urlopen(request)
+    serverDoc = minidom.parseString(server_content.read())
+    entryInfo = serverDoc.getElementsByTagName('entry')
+    key_nodes = entryInfo[0].getElementsByTagName('content')[0].getElementsByTagName('s:key')
+    nodes = filter(lambda node: node.attributes['name'].value == 'version', key_nodes)
+    version = nodes[0].firstChild.nodeValue
 
-        return(version)
+    return(version)
 
 def exitAlertScript(a):
-	if _DEBUG:
-		logDebug("Stopping: "+_MI_APP_NAME)
-		outputFileDebug.close()
-	if _LOG_ACTION:
-		outputFileAction.close()
-	sys.exit(a)
+    if _DEBUG:
+        logDebug("Stopping: "+_MI_APP_NAME)
+        outputFileDebug.close()
+    if _LOG_ACTION:
+        outputFileAction.close()
+    sys.exit(a)
 
 if __name__ == "__main__":
 
-	if _DEBUG:
-		logDebug("action=started")
+    if _DEBUG:
+        logDebug("action=started")
 
-# Define the source in Splunk for the stored credential
+    # Define the source in Splunk for the stored credential
 
-	splunkapp = "myadmin"
-	realm = 'googledocs'
-	username = 'googleusername'
-	spreadsheet_name = "Splunk Test"
+    splunkapp = "myadmin"
+    realm = 'googledocs'
+    username = 'googleusername'
+    spreadsheet_name = "Splunk Test"
 
-# Obtain the Splunk authentication session key
+    # Obtain the Splunk authentication session key
 
-        # read session key sent from splunkd 
-        sessionKey = sys.stdin.readline().strip()
+    # read session key sent from splunkd 
+    sessionKey = sys.stdin.readline().strip()
 
-        if len(sessionKey) == 0:
-        	logError("Did not receive a session key from splunkd. ")
-		exitAlertScript(_SYS_EXIT_FAILED_SPLUNK_AUTH)
+    if len(sessionKey) == 0:
+        logError("Did not receive a session key from splunkd. ")
+        exitAlertScript(_SYS_EXIT_FAILED_SPLUNK_AUTH)
 
-# Adjust the returned sessionKey text based on Splunk version
+    # Adjust the returned sessionKey text based on Splunk version
 
-        splunkVersion = getSplunkVersion(sessionKey)
-        if versiontuple(splunkVersion) < versiontuple("6.1.2"):
-                sessionKey = sessionKey[11:]
-        else:
-                sessionKey = urllib.unquote(sessionKey[11:]).decode('utf8')
+    splunkVersion = getSplunkVersion(sessionKey)
+    if versiontuple(splunkVersion) < versiontuple("6.1.2"):
+        sessionKey = sessionKey[11:]
+    else:
+        sessionKey = urllib.unquote(sessionKey[11:]).decode('utf8')
 
-        logDebug("sessionKey="+sessionKey)
-        logDebug("splunkVersion="+splunkVersion)
+    logDebug("sessionKey="+sessionKey)
+    logDebug("splunkVersion="+splunkVersion)
 
-# Define the Google credential
+    # Define the Google credential
 
-        googleCredential = credential(splunkapp,realm,username)
+    googleCredential = credential(splunkapp,realm,username)
 
-# Get the stored credential from Splunk
+    # Get the stored credential from Splunk
 
-	try:
-		googleCredential.getPassword(sessionKey)
-	except Exception, e:
-		logError("Splunk Credential Error: %s" % str(e))
-		exitAlertScript(_SYS_EXIT_FAILED_SPLUNK_AUTH)
+    try:
+        googleCredential.getPassword(sessionKey)
+    except Exception, e:
+        logError("Splunk Credential Error: %s" % str(e))
+        exitAlertScript(_SYS_EXIT_FAILED_SPLUNK_AUTH)
 
-# Active the Google Spreadsheet connection object	
-	try:
-		alert_spreadsheet = spreadsheet(googleCredential.username, googleCredential.password,spreadsheet_name)
-	except Exception, e:
-	        logError("Google Error: %s" % str(e))
-		exitAlertScript(_SYS_EXIT_FAILED_TP)	
+    # Active the Google Spreadsheet connection object   
+    try:
+        alert_spreadsheet = spreadsheet(googleCredential.username, googleCredential.password,spreadsheet_name)
+    except Exception, e:
+        logError("Google Error: %s" % str(e))
+        exitAlertScript(_SYS_EXIT_FAILED_TP)    
 
-# Upload the results to the Google Spreadsheet 
+    # Upload the results to the Google Spreadsheet 
 
-        alertEventsFile = os.environ['SPLUNK_ARG_8']
+    alertEventsFile = os.environ['SPLUNK_ARG_8']
 
-	try:
-		alert_spreadsheet.loadData(alertEventsFile)
-	except Exception, e:
-		logError("Target File Error: %s" % str(e))
-		exitAlertScript(_SYS_EXIT_FAILED_TARGET_FILE)
+    try:
+        alert_spreadsheet.loadData(alertEventsFile)
+    except Exception, e:
+        logError("Target File Error: %s" % str(e))
+        exitAlertScript(_SYS_EXIT_FAILED_TARGET_FILE)
 
-	exitAlertScript(0)
+    exitAlertScript(0)
 
--- GoogleSpreadsheet/gsheet.py
+++ GoogleSpreadsheet/gsheet.py
@@ -1,7 +1,7 @@
 """gsheet.py
-	Google Spreadsheet interface class
-	Note that right now the dictionary mapping is defined and handled here rather than in the alert script that calls it
-		see the loadData() function
+    Google Spreadsheet interface class
+    Note that right now the dictionary mapping is defined and handled here rather than in the alert script that calls it
+    see the loadData() function
 """
 
 import sys
@@ -12,148 +12,148 @@ import re, os
 
 class spreadsheet:
 
-	"""spreadsheet object: 
-		
-		Attributes:
-			username -- the google user name	
-			password -- the password to the google account 
-			name -- the document name
-			spreadsheetID -- the Google API spreadsheet ID retrieved by matching the document name
-			worksheetID -- the Google API worksheet ID within the spreadsheet, assumed to be first sheet
-	"""
+    """spreadsheet object: 
+        
+        Attributes:
+            username -- the google user name    
+            password -- the password to the google account 
+            name -- the document name
+            spreadsheetID -- the Google API spreadsheet ID retrieved by matching the document name
+            worksheetID -- the Google API worksheet ID within the spreadsheet, assumed to be first sheet
+    """
 
-	def __init__(self, username, password, name):	
-		self.username = username
-		self.password = password
-		self.name = name
-		self.client = ""
-		self.spreadsheetID = ""
-		self.worksheetID = ""
-		self.listFeed = ""
-		self.rows = ""
+    def __init__(self, username, password, name):   
+        self.username = username
+        self.password = password
+        self.name = name
+        self.client = ""
+        self.spreadsheetID = ""
+        self.worksheetID = ""
+        self.listFeed = ""
+        self.rows = ""
 
-		self.open()
-			
-	def __str__(self):
-		"""Function Override: Print spreadsheet contents 
-		"""
+        self.open()
+            
+    def __str__(self):
+        """Function Override: Print spreadsheet contents 
+        """
 
-		print 'Spreadsheet ID:%s' % (self.spreadsheetID)
-		print 'Worksheet ID:%s' % (self.worksheetID)
+        print 'Spreadsheet ID:%s' % (self.spreadsheetID)
+        print 'Worksheet ID:%s' % (self.worksheetID)
 
- 		for line in self.rows:
-			print line				
-		return("\r\n")
+        for line in self.rows:
+            print line              
+        return("\r\n")
 
-	def read (self):
-		"""Function: Pull in all the rows 
-		"""
+    def read (self):
+        """Function: Pull in all the rows 
+        """
 
-		self.rows = self.listFeed.entry
+        self.rows = self.listFeed.entry
 
-	def addData(self, dataDict):
-		"""Function: Insert the data in dict format to the google spreadsheet
-		"""
+    def addData(self, dataDict):
+        """Function: Insert the data in dict format to the google spreadsheet
+        """
 
-		entry = self.client.InsertRow(dataDict, self.spreadsheetID, self.worksheetID)
+        entry = self.client.InsertRow(dataDict, self.spreadsheetID, self.worksheetID)
 
-	def open(self):
-		"""Function: Open the google spreadsheet and read in the data 
-		"""
+    def open(self):
+        """Function: Open the google spreadsheet and read in the data 
+        """
 
-		# Connect to Google
-		self.client = gdata.spreadsheet.service.SpreadsheetsService()
-		self.client.email = self.username
-		self.client.password = self.password
-		self.client.source = 'spreadsheet.py' 
-		self.client.ProgrammaticLogin()
+        # Connect to Google
+        self.client = gdata.spreadsheet.service.SpreadsheetsService()
+        self.client.email = self.username
+        self.client.password = self.password
+        self.client.source = 'spreadsheet.py' 
+        self.client.ProgrammaticLogin()
 
-		q = gdata.spreadsheet.service.DocumentQuery()
-		q['title'] = self.name
-		q['title-exact'] = 'true'
-		feed = self.client.GetSpreadsheetsFeed(query=q)
-		self.spreadsheetID = feed.entry[0].id.text.rsplit('/',1)[1]
-		feed = self.client.GetWorksheetsFeed(self.spreadsheetID)
-		self.worksheetID = feed.entry[0].id.text.rsplit('/',1)[1]
-		self.listFeed = self.client.GetListFeed(self.spreadsheetID, self.worksheetID)
+        q = gdata.spreadsheet.service.DocumentQuery()
+        q['title'] = self.name
+        q['title-exact'] = 'true'
+        feed = self.client.GetSpreadsheetsFeed(query=q)
+        self.spreadsheetID = feed.entry[0].id.text.rsplit('/',1)[1]
+        feed = self.client.GetWorksheetsFeed(self.spreadsheetID)
+        self.worksheetID = feed.entry[0].id.text.rsplit('/',1)[1]
+        self.listFeed = self.client.GetListFeed(self.spreadsheetID, self.worksheetID)
 
-		try:
-			self.read()
+        try:
+            self.read()
 
-		except Exception, e:
-			raise Exception, "%s" % str(e)	
+        except Exception, e:
+            raise Exception, "%s" % str(e)  
 
-	def loadData(self, eventResultsPath, skip="yes"):
-		"""Function: take the gzip Splunk results file, extract and build the data dict for the spreadsheet
-			assumes the output from the Splunk alert matches the dict format defined here
-		"""
+    def loadData(self, eventResultsPath, skip="yes"):
+        """Function: take the gzip Splunk results file, extract and build the data dict for the spreadsheet
+            assumes the output from the Splunk alert matches the dict format defined here
+        """
 
-		import csv
-       		import gzip
-		import datetime
-	
-		filepath = eventResultsPath
-        	skipheader = skip
+        import csv
+        import gzip
+        import datetime
+    
+        filepath = eventResultsPath
+        skipheader = skip
 
-        	# Handle to the csv contents of the alerts events compressed file
+        # Handle to the csv contents of the alerts events compressed file
 
-        	try:
-        		eventContents = csv.reader(gzip.open(filepath, 'rb'))
+        try:
+            eventContents = csv.reader(gzip.open(filepath, 'rb'))
 
-        	except Exception, e:
-                	raise Exception, "%s" % str(e)
+        except Exception, e:
+            raise Exception, "%s" % str(e)
 
-        	eventIterator = iter(eventContents)
+        eventIterator = iter(eventContents)
 
-        	#skip header makes it skip past the first row to account for column header coming from Splunk search results
-        	if skipheader == "yes":
-        		eventIterator.next()
+        #skip header makes it skip past the first row to account for column header coming from Splunk search results
+        if skipheader == "yes":
+            eventIterator.next()
 
-        	# Build the alert results dict and add to the google spreadsheet
-        	for line in eventIterator:
-                	dataDict = {}
-			dateTime = datetime.datetime.fromtimestamp(float(line[0]))
-			dataDict['date'] = dateTime.strftime("%Y-%m-%d %I:%M:%S")
-			dataDict['ip'] = line[2]
-			dataDict['post'] = line[4]
-			self.addData(dataDict)
-		
+        # Build the alert results dict and add to the google spreadsheet
+        for line in eventIterator:
+            dataDict = {}
+            dateTime = datetime.datetime.fromtimestamp(float(line[0]))
+            dataDict['date'] = dateTime.strftime("%Y-%m-%d %I:%M:%S")
+            dataDict['ip'] = line[2]
+            dataDict['post'] = line[4]
+            self.addData(dataDict)
+        
 def main():
-	import getopt
-	
-	""" Define the google spreadsheet name for testing. A spreadsheet by this name must exist in the google account
-	"""
+    import getopt
+    
+    """ Define the google spreadsheet name for testing. A spreadsheet by this name must exist in the google account
+    """
 
-        name = "Splunk Test"
-        username = ''
-        password = ''
+    name = "Splunk Test"
+    username = ''
+    password = ''
 
- 	# parse command line options
-  	try:
-    		opts, args = getopt.getopt(sys.argv[1:], "", ["user=", "pw="])
-  	except getopt.error, msg:
-    		print 'python gsheet.py --user [username] --pw [password] '
-    		sys.exit(2)
+    # parse command line options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "", ["user=", "pw="])
+    except getopt.error, msg:
+        print 'python gsheet.py --user [username] --pw [password] '
+        sys.exit(2)
 
-  	# Process options
-  	for o, a in opts:
-    		if o == "--user":
-      			username = a
-    		elif o == "--pw":
-      			password = a
+    # Process options
+    for o, a in opts:
+        if o == "--user":
+            username = a
+        elif o == "--pw":
+            password = a
 
-  	if username == '' or password == '':
-    		print 'python gsheet.py --user [username] --pw [password] '
-    		sys.exit(2)
-	
-       	test_spreadsheet = spreadsheet(username, password, name)
+    if username == '' or password == '':
+        print 'python gsheet.py --user [username] --pw [password] '
+        sys.exit(2)
+    
+    test_spreadsheet = spreadsheet(username, password, name)
 
-        print test_spreadsheet
+    print test_spreadsheet
 
 if __name__ == "__main__":
 
-	try:
-		main()
-	except Exception, e:
-		raise Exception, "%s" % str(e)
+    try:
+        main()
+    except Exception, e:
+        raise Exception, "%s" % str(e)
 
